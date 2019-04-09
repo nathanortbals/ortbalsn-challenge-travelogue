27611898eb89af7fe6cf9da3b0ec22364389f848
Nathan Ortbals <nathan.ortbals@gmail.coma>
2019-04-09 13:43:35 -0500

    Initial Commit

---
+++ ortbalsn-challenge-travelogue/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ortbalsn-challenge-travelogue
+//
+//  Created by Nathan Ortbals on 4/9/19.
+//  Copyright © 2019 nathanortbals. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
